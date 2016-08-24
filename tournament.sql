d5d767443f194641cd7dbc552d008902a5812f93
harikrish narasiman <Harikrishnan_Narasimhan@cable.comcast.com>
2016-08-23 22:02:39 -0400

    opponent match wins

--- tournament.sql
+++ tournament.sql
@@ -38,8 +38,8 @@ CREATE TABLE player_byes(
 );
 
 CREATE VIEW standings AS
-SELECT players.t_id, players.id, players.name,
-(SELECT count(*) FROM match_results WHERE players.id = match_results.winner and match_results.draw = FALSE AND ) AS wins,
+SELECT players.id, players.name,
+(SELECT count(*) FROM match_results WHERE players.id = match_results.winner and match_results.draw = FALSE) AS wins,
 (SELECT count(*) FROM match_results WHERE players.id = match_results.winner OR players.id = match_results.loser ) AS matches,
-(SELECT COALESCE(SUM(points), 0) FROM player_match_points WHERE players.id = player_match_points.player_id) AS points
-FROM players ORDER BY wins DESC;
+(SELECT COALESCE(SUM(points), 0) FROM player_match_points WHERE players.id = player_match_points.player_id) AS total_points,
+(SELECT COALESCE(SUM(points), 0) FROM player_match_points WHERE match_id IN (SELECT match_id FROM match_results WHERE loser = players.id) OR match_id IN (SELECT match_id FROM match_results WHERE winner = players.id) and player_id <> players.id) AS omw FROM players ORDER BY wins DESC, omw DESC;
